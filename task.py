eabc224ddc3249dcc278dddd6c165e4ac0ede46c
ynhacler <zzh1989@126.com>
2014-01-04 17:16:30 +0800

    Update task.py
    加了注释 2014.1.4-17-00

--- task.py
+++ task.py
@@ -24,27 +24,39 @@ except ImportError:
 from datetime import datetime,timedelta
 from time import mktime
 
-
+#jinja2初始化环境
 tmp_env = Environment(loader=FileSystemLoader(config.template_path))
 
 PICTURE_RE = re.compile(r'src="(http://[-_+=a-zA-Z0-9/.]+(jpe?g|gif|png)[?]?[^"]*)"')
 
+
+#把rss里的信息分析后存入数据库
 def sync_feed(slug, check_exist=True):
     """
     抓取feed文本内容
     """
+    #日志
     logging.info('task.sync_feed: slug->%s ' %(slug))
-    db = m.Session()
+    db = m.Session()#数据库的session，不是HTTP的。
+    
     feed = db.query(m.Feed).filter_by(slug=slug).first()
+    
+    
     if feed is None: 
         logging.warning('feed not found , slug->%s' %(slug))
         db.close()
         return 
+    
+    #用feedsparser取得rss
     data = feedparser.parse(feed.url)
-    if not data.entries: return 
+    if not data.entries: return #内容为空就返回
+
+    #时间戳
     feed_timestamp = get_timestamp(data.feed) or \
             datetime.now()
     fetch_timestamp = datetime.now()
+    
+    #第三个参数是it is returned when the attribute doesn't exist
     feed_title = getattr(data.feed,'title',feed.name)
     for entry in data.entries:
         title = getattr(entry,'title','')
@@ -63,6 +75,8 @@ def sync_feed(slug, check_exist=True):
             content = u'\n'.join([c.value for c in entry.content])
         else:
             content = getattr(entry,'summary','')
+            
+        #存入数据库
         new_entry = m.Entry(
                 title = title,
                 time_stamp = time_stamp,
@@ -72,6 +86,7 @@ def sync_feed(slug, check_exist=True):
                 content = content,
                 fid = feed.id)
         db.add(new_entry)
+        
     feed.sync_time = datetime.now()
     try:
         db.commit()
@@ -91,13 +106,22 @@ def fetch_img(feed_slug, eid, force=False):
     """
     获取图片
     """
+    #eid文档id号
+    
+    #日志
     logging.info('task.fetch_image:entry id->%s ' %(str(eid)))
+    
+    #数据库session
     db = m.Session()
     feed = db.query(m.Feed).filter_by(slug=feed_slug).first()
     if feed is not None and not feed.fetch_image:
         db.close()
         return
+    
+    #根据文章id提取rss
     entry = db.query(m.Entry).filter_by(id=eid).first()
+    
+    #rss是否合法或存在图片地址
     if entry is None or (entry.got_img and not force):
         logging.warning('entry not found , entry id->%s \n'
                 'or encounter error when I try fetch image for it last time.' %(eid))
@@ -106,19 +130,30 @@ def fetch_img(feed_slug, eid, force=False):
 
     img_serial = 0
     total_size = 0
+    
+    #解析并提取图片地址
     matches = list(PICTURE_RE.finditer(entry.content, re.I)) 
+    
+    #创建存储图片的文件夹
     if matches and not path.exists(path.join(config.img_storage_path,str(entry.id))):
         os.mkdir(path.join(config.img_storage_path,str(entry.id)))
+        
     for match in matches:
+        #图片类型
         if match.group(2).strip().lower() == 'gif':
             extend, media_type= '.gif', 'image/gif'
         elif match.group(2).strip().lower() == 'png':
             extend, media_type = '.png', 'image/png'
         else:
             extend, media_type = '.jpg', 'image/jpeg'
+            
+        #求md5
         file_name = md5(feed_slug+str(entry.id)+str(img_serial)).hexdigest()
+        
+        #创建图片文件
         with open(path.join(config.img_storage_path,str(entry.id),file_name+extend), 'wb') as f: 
             try:
+                #读图片
                 resopne = urlopen(match.group(1))
             except Exception, e:
                 logging.warning("encounter exception %s" %(str(e)))
@@ -127,12 +162,19 @@ def fetch_img(feed_slug, eid, force=False):
             if resopne.getcode() != 200:
                 logging.warning("response status %s \n Can not fetch img from src: %s",response.status, match.group(1))
                 continue
+            #写入图片
             f.write(resopne.read())
         #endwith
+        
+        #得到所有图片的大小
         total_size += path.getsize(path.join(config.img_storage_path,str(entry.id),file_name+extend))
+        
+        #图片信息存入数据库
         img = m.Image(eid = entry.id, serial_no = img_serial, file_name = file_name, extend = extend)
         db.add(img)
         img_serial += 1
+        
+        #图片体积过大就报错
         if total_size >= config.img_size_per_entry:
             logging.warning('entry %s contain too much image size: %sM%sK' %( eid, total_size/1024/1024, total_size%1024))
             break
@@ -148,8 +190,13 @@ def fetch_img(feed_slug, eid, force=False):
 
 
 def push(uid):
+    
+    #日志
     logging.info('task.push:user id->%s ' %(str(uid)))
+    #数据库session
     db = m.Session()
+    
+    #查询用户信息
     user = db.query(m.User).filter_by(id=uid).first()
     if user is None:
         logging.warning(u'task.push user not found uid->%s' %(str(uid)))
@@ -158,25 +205,38 @@ def push(uid):
     play_order = 1
     feeds = []
     total_image_size  =  0
+    
+    #把用户订阅的rss提取出来
     #for feed in user.feeds:
     for subscription in user.subscription_relation:
         if not subscription.active:
             continue
+        
+        #订阅的rss
         feed = subscription.feed
+        
+        #用户刚推送过就不推送
         if user.push_timestamp > feed.sync_time:
             continue
+        
+        #提取文章
         entries = [ e for e in feed.entries \
                 if e.fetch_timestamp > subscription.subscribe_timestamp \
                 and len(e.images) <4]
+                
         if len(entries) <= 0:
             continue
+        
         latest_timestamp = max([ e.fetch_timestamp for e in entries])
+        
         play_order += 1
         f = {'fid': str(feed.id),
                 'name': feed.name,
                 'play_order': play_order,
                 'entries': [],
             }
+            
+        #处理文章中的章节
         for e in entries:
             play_order += 1
             entry = { 'title': e.title,
@@ -187,27 +247,39 @@ def push(uid):
                     'eid': str(e.id),
                     }
             if len(e.images) ==  0 or not feed.fetch_image or total_image_size >= config.img_size_per_push:
+                #没有图片
                     entry['content'] = e.content
             else:
+                #有图片
                 img_map = {}
                 for i,match in enumerate(PICTURE_RE.finditer(e.content,re.I)):
 
                     #计算添加的图片大小
                     total_image_size += path.getsize(path.join(config.img_storage_path,
                         str(e.id), e.images[i].file_name + e.images[i].extend))
+                    
+                    #图片总体积过大就不放入
                     if total_image_size >= config.img_size_per_push:
                         break
 
+                    #取得图片地址
                     img_path = path.join(config.img_storage_dir, 
                             str(e.id),
                             e.images[i].file_name + e.images[i].extend
                             )
+                            
+                    #把http地址换成本机图片地址
                     img_map[match.group(1)] = 'src="%s"' %(img_path)
+                
                 entry['content'] = PICTURE_RE.sub( lambda match: img_map[match.group(1)], e.content)
+                
             f['entries'].append(entry)
             subscription.subscribe_timestamp = latest_timestamp
         #endfor
+        
+        #文章放入订阅中
         feeds.append(f)
+        
     if len(feeds) == 0:
         logging.info('task.push:user id->%s has no update' %(str(uid)))
         db.close()
